385a46b798b5980e9ab622738d0ff94387dc3ed1
kristak313 <krista@alpinspire.com>
2015-06-07 16:41:59 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,1 +1,26 @@
+## Read full file
+allData <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+allData$Date <- as.Date(allData$Date, format="%d/%m/%Y")
 
+## Subsetting collected data as specified
+data <- subset(allData, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(allData)
+
+## Convert dates as specified
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Create plot 3
+with(data, {
+    plot(Sub_metering_1~Datetime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Copy plot to PNG file with specified height and width
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
